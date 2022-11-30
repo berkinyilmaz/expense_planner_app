7a6a66c4634264842e0e9eca5f662cec1976322a
Berkin Yılmaz <62833425+berkinyilmaz@users.noreply.github.com>
2022-11-30 13:51:48 +0300

    input nums limited and amount input keyboard changed

--- lib/widgets/new_transactions.dart
+++ lib/widgets/new_transactions.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
- //the new transaction widget
+//the new transaction widget
 
 class NewTransaction extends StatelessWidget {
   final Function addTx;
@@ -7,6 +7,20 @@ class NewTransaction extends StatelessWidget {
   final amountController = TextEditingController();
 
   NewTransaction(this.addTx);
+
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+
+    addTx(
+      enteredTitle,
+      enteredAmount,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,10 +37,13 @@ class NewTransaction extends StatelessWidget {
             TextField(
               decoration: InputDecoration(labelText: 'Title'),
               controller: titleController,
+              onSubmitted: (_) => submitData,
             ),
             TextField(
               decoration: InputDecoration(labelText: 'Amount'),
               controller: amountController,
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => submitData,
             ),
             TextButton(
               child: Text(
@@ -36,12 +53,7 @@ class NewTransaction extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                     fontSize: 15),
               ),
-              onPressed: () {
-                addTx(
-                  titleController.text,
-                  double.parse(amountController.text),
-                );
-              },
+              onPressed: submitData,
             ),
           ],
         ),
